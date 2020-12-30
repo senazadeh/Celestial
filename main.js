3ab3895130813a7747b004e6428b13c69802aa76
senazadeh <46635460+senazadeh@users.noreply.github.com>
2020-12-30 10:40:21 -0500

    Updating time

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 
-var d = new Date();
-document.getElementById("demo").innerHTML = d;
+function updateTime(){
+    var currentTime = new Date()
+    var month = currentTime.getUTCMonth()
+    var day = currentTime.getDate()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    var seconds = currentTime.getSeconds()
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    if (seconds < 10){
+        seconds = "0" + seconds
+    }
+    var t_str = month + " " + day + " " + hours + ":" + minutes + ":" + seconds + " ";
+    if (hours > 11){
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+    }
+    document.getElementById('time_span').innerHTML = t_str;
+}
+setInterval(updateTime, 1000);
